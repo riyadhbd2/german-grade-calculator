d726865f46929339ee984b2d8cc6325bab18e8d8
Easir Arafat <riyadhbd2@Easirs-MacBook-Pro.local>
2024-01-26 11:18:33 +0100

    fixed decimal number 2

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -6,13 +6,15 @@ const Value = () => {
   const [result, setResult] = useState("");
   const [final, setFinal] = useState("");
 
+  // console.log(newFinal);
+
   const handleSubmit = () => {
     const result1 = parseFloat(maximum) - parseFloat(result);
     const result2 = parseFloat(maximum) - parseFloat(minimum);
     const result3 = result1 / result2;
     const result4 = result3 * 3;
     const result5 = result4 + 1;
-    setFinal(result5);
+    setFinal(result5.toFixed(2));
   };
 
   //   (Nmax – Nd / Nmax – Nmin)*3 + 1
